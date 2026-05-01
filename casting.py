1d1ca932268b2906dd6187a370056b47541f0738
rockygarg-crypto <rockygarg74@gmail.com>
2026-05-02 00:01:41 +0530

    casting .py

--- casting.py
+++ casting.py
@@ -1,4 +1,12 @@
-num1="50"
-num2="20"
-res=int(num1)+int(num2)
+#num1="50"
+#num2="20"
+#res=int(num1)+int(num2)
+#print(res)
+num1=50
+num2=20
+print(type(num1),type(num2))
+res=str(num1)+str(num2)
 print(res)
+
+
+
