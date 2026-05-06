06c427e034c1ae7b5a2e78c075ccd2473a519ec1
rockygarg-crypto <rockygarg74@gmail.com>
2026-05-06 07:48:40 +0530

    new.py

--- new.py
+++ new.py
@@ -1,5 +1,6 @@
-str1 = "this is assingment for python"
-find = "for"
-print(find in str1)
-
-
+rocky_age = 26
+yash_age = 24
+if rocky_age >= yash_age :
+    print("yes rocky age is more than yash")
+else:
+    print("no rocky age is more than yash")
