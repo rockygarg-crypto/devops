c2c130235b2b30c28a3be99e363f804b0fc83af5
rockygarg-crypto <rockygarg74@gmail.com>
2026-05-12 10:41:30 +0530

    casting.py

--- casting.py
+++ casting.py
@@ -1,15 +1,37 @@
-#wap to print the last digit of and number is 456735
+# #wap to print the last digit of and number is 456735
 
-number = 45009
-#print(number%10000)
+# number = 45009
+# #print(number%10000)
 
 
-num1=3
-num2=9
-num3=12
-#wap to find largest
-#wap to find smallest
+# num1=3
+# num2=9
+# num3=12
+# #wap to find largest
+# #wap to find smallest
  
-if num3>=num1 and num3>=num2:
-#    print(f"{num3} is greatest") 
+# if num3>=num1 and num3>=num2:
+# #    print(f"{num3} is greatest") 
 
+#wap to count all the vowels from give string : "this is deveops batch".
+
+# str1="this is devops batch"
+# v_count=0
+# c_count=0
+# for i in str1:
+#     if i in "aeiou": 
+#        v_count+=1
+#     else:
+#        c_count+=1
+     
+# print(v_count) 
+# print(c_count)     
+
+
+# wap to print your name is reverse formate.
+
+# name="rocky"
+# rev=""
+# for i in name:
+#     rev=i+rev
+# print(rev)
--- new.py
+++ new.py
@@ -1,6 +1,20 @@
-rocky_age = 26
-yash_age = 24
-if rocky_age >= yash_age :
-    print("yes rocky age is more than yash")
-else:
-    print("no rocky age is more than yash")
+# range_start = int(input("enter start point :"))
+# range_end = int(input("enter end point :"))
+# for i in range(range_start , range_end):
+#     if i % 2 == 0 and i % 3 == 0 :
+#         print(i)
+
+
+
+# num = int(input("enter your number : "))
+# for i in range(1,11):
+#     print(f"{num} * {i} : {num * i}")
+
+
+# num = int(input("enter your name :"))
+# for i in range(10,0,-1):
+#     print(f"{num} * {i} : {num * i}")
+
+
+
+            
